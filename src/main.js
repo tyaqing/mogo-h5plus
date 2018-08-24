ed7446320f832695d9983e576dad8acb37520dc0
滕亚庆 <626019610@qq.com>
2018-08-24 09:15:56 +0800

    编译apk,增加上热更新

--- src/main.js
+++ src/main.js
@@ -6,39 +6,38 @@ import "./utils/common";
 import App from "./App";
 const appId = "com.femirror.mogoh5";
 const updateUrl = FemirrorAPI + `/public/app/checkUpdate?bundleId=${appId}`;
-// checkUpdate(updateUrl);
-// hotfix({
-//   url: updateUrl,
-//   before(data) {
-//     // 确认安装
-//     return new Promise(resolve => {
-//       plus.nativeUI.confirm(data.title, (selected) => {
-//         if (selected.index === 0) {
-//           plus.nativeUI.showWaiting("下载更新文件...");
-//           resolve(data);
-//         }
-//       }, {
-//           title: data.description,
-//           buttons: ["确认更新", "取消"],
-//           verticalAlign: "bottom"
-//         });
-//     });
-//   },
-//   success() {
-//     console.log('安装成功')
-//     plus.runtime.restart(); // 重启app 
-//     plus.nativeUI.closeWaiting();
-//   },
-//   error(e) {
-//     // 错误显示
-//     plus.nativeUI.closeWaiting();
-//     console.log("安装wgt文件失败[" + e.code + "]：" + e.message);
-//     plus.nativeUI.alert("安装wgt文件失败[" + e.code + "]：" + e.message);
-//   },
-//   onProgress(p) {
-//     console.log(p) // 下载进度
-//   }
-// });
+hotfix({
+  url: updateUrl,
+  before(data) {
+    // 确认安装
+    return new Promise(resolve => {
+      plus.nativeUI.confirm(data.description, (selected) => {
+        if (selected.index === 0) {
+          plus.nativeUI.showWaiting("下载更新文件...");
+          resolve(data);
+        }
+      }, {
+          title: data.title,
+          buttons: ["确认更新", "取消"],
+          verticalAlign: "bottom"
+        });
+    });
+  },
+  success() {
+    console.log('安装成功')
+    plus.runtime.restart(); // 重启app 
+    plus.nativeUI.closeWaiting();
+  },
+  error(e) {
+    // 错误显示
+    plus.nativeUI.closeWaiting();
+    console.log("安装wgt文件失败[" + e.code + "]：" + e.message);
+    plus.nativeUI.alert("安装wgt文件失败[" + e.code + "]：" + e.message);
+  },
+  onProgress(p) {
+    console.log(p) // 下载进度
+  }
+});
 
 Vue.config.productionTip = false;
 
